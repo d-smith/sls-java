a71d2be792b6fe721c44327f88378d0fd334f80a
d-smith <doug.smith.mail@gmail.com>
2021-08-28 21:43:32 +0000

    Added kafka prop read and topic create

--- src/main/java/com/ds/Handler.java
+++ src/main/java/com/ds/Handler.java
@@ -8,29 +8,63 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.util.Map;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.Producer;
 
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 import java.util.Set;
 
+import java.util.Collections;
+import java.util.Map;
+import java.util.Optional;
+
+import java.util.concurrent.ExecutionException;
+import org.apache.kafka.common.errors.TopicExistsException;
+
+
+
 // Handler value: example.Handler
 public class Handler implements RequestHandler<Map<String,String>, String>{
+  
+  public static void createTopic(final String topic,
+                          final Properties cloudConfig) {
+      final NewTopic newTopic = new NewTopic(topic, Optional.empty(), Optional.empty());
+      try (final AdminClient adminClient = AdminClient.create(cloudConfig)) {
+          adminClient.createTopics(Collections.singletonList(newTopic)).all().get();
+      } catch (final InterruptedException | ExecutionException e) {
+          // Ignore if TopicExistsException, which may be valid if topic exists
+          if (!(e.getCause() instanceof TopicExistsException)) {
+              throw new RuntimeException(e);
+          }
+      }
+  }
   
   public Handler() {
     System.out.println("It is constructed...");
     
     
      try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
-       Properties prop = new Properties();
+       Properties props = new Properties();
 
             if (input == null) {
                 System.out.println("Sorry, unable to find " + "config.properties");
                 return;
             }
 
-            prop.load(input);
-            prop.keySet().forEach(x -> System.out.println(x));
+            props.load(input);
+            props.keySet().forEach(x -> System.out.println(x));
+            
+            createTopic("sample", props);
+            
      } catch (IOException ex) {
             ex.printStackTrace();
         }
