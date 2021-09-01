41a96dcad0b2fcb5f06933ada5e21e6934aa7cd4
d-smith <doug.smith.mail@gmail.com>
2021-09-01 14:12:00 -0700

    reformatted code

--- src/main/java/com/ds/Handler.java
+++ src/main/java/com/ds/Handler.java
@@ -8,6 +8,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.util.Map;
+
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
@@ -18,6 +19,7 @@ import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
@@ -28,38 +30,38 @@ import java.util.Map;
 import java.util.Optional;
 
 import java.util.concurrent.ExecutionException;
+
 import org.apache.kafka.common.errors.TopicExistsException;
 
 import com.ds.DataRecord;
 
 
-
 // Handler value: example.Handler
-public class Handler implements RequestHandler<Map<String,String>, String>{
-  
-  private static final String TOPIC = "sample";
-  
-  Producer<String, DataRecord> producer;
-  
-  public static void createTopic(final String topic,
-                          final Properties cloudConfig) {
-      final NewTopic newTopic = new NewTopic(topic, Optional.empty(), Optional.empty());
-      try (final AdminClient adminClient = AdminClient.create(cloudConfig)) {
-          adminClient.createTopics(Collections.singletonList(newTopic)).all().get();
-      } catch (final InterruptedException | ExecutionException e) {
-          // Ignore if TopicExistsException, which may be valid if topic exists
-          if (!(e.getCause() instanceof TopicExistsException)) {
-              throw new RuntimeException(e);
-          }
-      }
-  }
-  
-  public Handler() {
-    System.out.println("It is constructed...");
-    
-    
-     try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
-       Properties props = new Properties();
+public class Handler implements RequestHandler<Map<String, String>, String> {
+
+    private static final String TOPIC = "sample";
+
+    Producer<String, DataRecord> producer;
+
+    public static void createTopic(final String topic,
+                                   final Properties cloudConfig) {
+        final NewTopic newTopic = new NewTopic(topic, Optional.empty(), Optional.empty());
+        try (final AdminClient adminClient = AdminClient.create(cloudConfig)) {
+            adminClient.createTopics(Collections.singletonList(newTopic)).all().get();
+        } catch (final InterruptedException | ExecutionException e) {
+            // Ignore if TopicExistsException, which may be valid if topic exists
+            if (!(e.getCause() instanceof TopicExistsException)) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public Handler() {
+        System.out.println("It is constructed...");
+
+
+        try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
+            Properties props = new Properties();
 
             if (input == null) {
                 System.out.println("Sorry, unable to find " + "config.properties");
@@ -67,54 +69,54 @@ public class Handler implements RequestHandler<Map<String,String>, String>{
             }
 
             props.load(input);
-            
+
             // Add additional properties.
             props.put(ProducerConfig.ACKS_CONFIG, "all");
             props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
             props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaJsonSerializer");
-    
-    
+
+
             props.keySet().forEach(x -> System.out.println(x));
-            
+
             createTopic(TOPIC, props);
-            
+
             producer = new KafkaProducer<String, DataRecord>(props);
-            
-     } catch (IOException ex) {
+
+        } catch (IOException ex) {
             ex.printStackTrace();
         }
-    
-    
-  }
-  
-  Gson gson = new GsonBuilder().setPrettyPrinting().create();
-  @Override
-  public String handleRequest(Map<String,String> event, Context context)
-  {
-    LambdaLogger logger = context.getLogger();
-    String response = new String("200 OK");
-    // log execution details
-    logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
-    logger.log("CONTEXT: " + gson.toJson(context));
-    // process event
-    logger.log("EVENT: " + gson.toJson(event));
-    logger.log("EVENT TYPE: " + event.getClass().toString());
-    
-    DataRecord record = new DataRecord(event.toString());
 
-      producer.send(new ProducerRecord<String, DataRecord>(TOPIC, null, record), new Callback() {
-          @Override
-          public void onCompletion(RecordMetadata m, Exception e) {
-            if (e != null) {
-              e.printStackTrace();
-            } else {
-              System.out.printf("Produced record to topic %s partition [%d] @ offset %d%n", m.topic(), m.partition(), m.offset());
+
+    }
+
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    @Override
+    public String handleRequest(Map<String, String> event, Context context) {
+        LambdaLogger logger = context.getLogger();
+        String response = new String("200 OK");
+        // log execution details
+        logger.log("ENVIRONMENT VARIABLES: " + gson.toJson(System.getenv()));
+        logger.log("CONTEXT: " + gson.toJson(context));
+        // process event
+        logger.log("EVENT: " + gson.toJson(event));
+        logger.log("EVENT TYPE: " + event.getClass().toString());
+
+        DataRecord record = new DataRecord(event.toString());
+
+        producer.send(new ProducerRecord<String, DataRecord>(TOPIC, null, record), new Callback() {
+            @Override
+            public void onCompletion(RecordMetadata m, Exception e) {
+                if (e != null) {
+                    e.printStackTrace();
+                } else {
+                    System.out.printf("Produced record to topic %s partition [%d] @ offset %d%n", m.topic(), m.partition(), m.offset());
+                }
             }
-          }
-      });
-    
-     producer.flush();
-     
-    return response;
-  }
+        });
+
+        producer.flush();
+
+        return response;
+    }
 }
