410cb1dc8ddecd44d39b8761860424d98b976374
d-smith <doug.smith.mail@gmail.com>
2021-09-05 08:57:46 -0700

    more reader specific updates

--- src/main/java/com/ds/Reader.java
+++ src/main/java/com/ds/Reader.java
@@ -3,16 +3,22 @@ package com.ds;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Map;
 import java.util.Properties;
 
 public class Reader implements RequestHandler<Map<String, String>, String> {
 
+    private Consumer<Long, String> consumer;
+    private static final String TOPIC = "sample";
 
     public Reader() {
         try (InputStream input = getClass().getClassLoader().getResourceAsStream("config.properties")) {
@@ -26,13 +32,16 @@ public class Reader implements RequestHandler<Map<String, String>, String> {
             props.load(input);
 
             // Add additional properties.
-            props.put(ProducerConfig.ACKS_CONFIG, "all");
-            props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
-            props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaJsonSerializer");
+            //props.put(ProducerConfig.ACKS_CONFIG, "all");
 
+            props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+            props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "io.confluent.kafka.serializers.KafkaJsonDeserializer");
+            props.put(ConsumerConfig.GROUP_ID_CONFIG, "KafkaExampleConsumer");
 
             props.keySet().forEach(x -> System.out.println(x));
 
+            consumer = new KafkaConsumer<>(props);
+            consumer.subscribe(Arrays.asList(TOPIC));
 
         } catch (IOException ex) {
             ex.printStackTrace();
